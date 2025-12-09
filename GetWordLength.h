0d595441c0442c325c5ceeae4464730523cf5a98
tegz <144906350+zizzytv@users.noreply.github.com>
2025-12-09 21:47:55 +0000

    Delete GetWordLength.h

--- GetWordLength.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "textScale.h"
-#include "ProcessTestFile.h"
-#include "serial.h"
-#include "WordIntoGCode.h"
-#include "EndRobotDrawing.h"
-
-int GetWordLength(FILE *file, double scale); // Function prototype
